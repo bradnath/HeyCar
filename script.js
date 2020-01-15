7b4fac1aa54801316af7df0b492d9248c89a2496
bradn <nathansonbrad@gmail.com>
2020-01-15 06:38:32 -0500

    Print first question
    Properly link external .js file and print the details for the first question

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-/* Retrieve the Entry Point */
+/* Retrieve the Entry Point 
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://polls.apiblueprint.org/');
@@ -10,11 +9,13 @@ request.onreadystatechange = function () {
     console.log('Headers:', this.getAllResponseHeaders());
     console.log('Body:', this.responseText);
   }
+
+  var 
 };
 
 request.send();
 
-/* View a question Detail */
+/* View a question Detail 
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://polls.apiblueprint.org/questions/1');
@@ -32,7 +33,7 @@ request.onreadystatechange = function () {
 
 request.send();
 
-/* Vote on a choice */
+/* Vote on a choice 
 var request = new XMLHttpRequest();
 
 request.open('POST', 'https://polls.apiblueprint.org/questions/1/choices/1');
@@ -47,7 +48,7 @@ request.onreadystatechange = function () {
 
 request.send();
 
-/* List all questions */
+/* List all questions 
 var request = new XMLHttpRequest();
 
 request.open('GET', 'https://polls.apiblueprint.org/questions?');
@@ -60,4 +61,46 @@ request.onreadystatechange = function () {
   }
 };
 
-request.send();
+request.send(); 
+
+$.getJSON(
+  "https://polls.apiblueprint.org/", function(data){
+    console.log(data);
+  }
+);*/
+
+
+
+$.getJSON(
+  "https://polls.apiblueprint.org/questions/1", function(data){
+    //console.log(data);
+    //console.log(data.question);
+    //console.log(data.choices[3]);
+    document.getElementById('questionA').innerHTML = data.question;
+    document.getElementById('published-at').insertAdjacentHTML('beforeend', data.published_at);
+    document.getElementById('length').insertAdjacentHTML('beforeend', data.choices.length);
+    }
+);
+
+//var question = data.question 
+
+/*
+var numberOfQuestions = 4;
+var currentQuestion;
+for (i = 0; i < numberOfQuestions; i++) {
+  currentQuestion = document.getElementById('question1');
+  console.log(currentQuestion);
+}
+*/
+
+/*var text = document.getElementById('question1');
+
+text.innerHTML = 'HELLO!!!';
+
+console.log(text);*/
+
+
+
+//console.log(questionContainer)
+
+//pleasee.insertAdjacentHTML('beforeend', 'testing');
